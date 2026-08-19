912da2c6e2c43294d5ca70fc8b07ebae66b29e19
hufnag <hufnagel.manuel@gmail.com>
2026-08-19 15:20:27 +0200

    Fixes

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,18 @@ return {
 			"nvim-tree/nvim-web-devicons", -- optional, but recommended
 		},
 		lazy = false, -- neo-tree will lazily load itself
+		opts = {
+			default_component_configs = {
+				diagnostics = {
+					highlights = {
+						hint = "DiagnosticHint",
+						info = "DiagnosticInfo",
+						warn = "DiagnosticWarn",
+						error = "DiagnosticError",
+					},
+				},
+			},
+		},
 		keys = {
 			{ "ü", "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
 		},
